b999cda3c6d5f034644ac68e2d791c74483549da
Pankaj <pankajbarhate13@gmail.com>
2018-10-09 13:39:14 +0530

    add social authentication

---
+++ auth/twitter.js
@@ -0,0 +1,48 @@
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+
+var User = require('../server/models/user');
+var config = require('../server/config');
+var init = require('../auth/init');
+
+passport.use(new TwitterStrategy({
+    consumerKey: config.twitter.consumerKey,
+    consumerSecret: config.twitter.consumerSecret,
+    callbackURL: config.twitter.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+
+    console.log("========================")
+    console.log(profile)
+    console.log("========================")
+
+    var searchQuery = {
+      name: profile.displayName
+    };
+
+    var updates = {
+      name: profile.displayName,
+      someID: profile.id
+    };
+
+    var options = {
+      upsert: true
+    };
+
+    // update the user if s/he exists or add a new user
+    User.findOneAndUpdate(searchQuery, updates, options, function(err, user) {
+      if(err) {
+        return done(err);
+      } else {
+        return done(null, user);
+      }
+    });
+  }
+
+));
+
+// serialize user into the session
+init();
+
+
+module.exports = passport;
---
+++ server/config.js
@@ -0,0 +1,14 @@
+var ids = {
+  linkedin: {
+    clientID: '75xxxxxxxxxxxx',
+    clientSecret: 'ZNXXXXXXXXXXX',
+    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+  },
+  twitter: {
+    consumerKey: 'NaxXXXXXXXxXXXXXXX9r',
+    consumerSecret: 'XXxXXXXXXXXXXXXXXXXXXXXXXXXXXXxx',
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  }
+};
+
+module.exports = ids;
